2994bdc4f4c699d270e02c710c58a8fbc2acb20e
Dominique Guardiola <contact@quinode.fr>
2012-04-28 19:49:18 +0200

    PEP8

--- coop_tag/widgets.py
+++ coop_tag/widgets.py
@@ -1,28 +1,29 @@
-from django.forms.widgets import CheckboxSelectMultiple,CheckboxInput
+from django.forms.widgets import CheckboxSelectMultiple, CheckboxInput
 from django.utils.encoding import force_unicode
-from itertools import chain
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
+
 
 class GroupedCheckboxSelectMultiple(CheckboxSelectMultiple):
     class Media:
         js = ('js/autocolumn.js',)
     
     def render(self, name, value, attrs=None, choices=()):
-        if value is None: value = []
+        if value is None: 
+            value = []
         has_id = attrs and 'id' in attrs
         final_attrs = self.build_attrs(attrs, name=name)
         # Normalize to strings
         str_values = set([force_unicode(v) for v in value])
-        output=[u'<div class="groupbox">']
-        for z,g in enumerate(self.choices):
+        output = [u'<div class="groupbox">']
+        for z, g in enumerate(self.choices):
             output.append(u'<div class="groupbox-header">%s</div>' % g[0])
             output.append(u'<ul class="groupbox-list">')
             for i, (option_value, option_label) in enumerate(g[1]):
                 # If an ID attribute was given, add a numeric index as a suffix,
                 # so that the checkboxes don't all have the same ID attribute.
                 if has_id:
-                    final_attrs = dict(final_attrs, id='%s_%s_%s' % (attrs['id'],z, i))
+                    final_attrs = dict(final_attrs, id='%s_%s_%s' % (attrs['id'], z, i))
                     label_for = u' for="%s"' % final_attrs['id']
                 else:
                     label_for = ''
@@ -42,3 +43,4 @@ class GroupedCheckboxSelectMultiple(CheckboxSelectMultiple):
             id_ += '_0'
         return id_
     id_for_label = classmethod(id_for_label)
+    
