bfd6f26fd962639a27d5dc9bb382a3336bc0d389
Dominique Guardiola <dguardiola@quinode.fr>
2012-05-06 20:36:46 +0200

    PEP 8

--- coop_tag/models.py
+++ coop_tag/models.py
@@ -10,12 +10,13 @@ from positions.fields import PositionField
 
 class TagCategory(models.Model):
     label = models.CharField(_(u'label'), max_length=100)
-    uri = models.CharField(_(u'tag URI'),blank=True,max_length=250,editable=False)
-    slug    = exfields.AutoSlugField(populate_from='label',blank=True)
+    uri = models.CharField(_(u'tag URI'), blank=True, max_length=250, editable=False)
+    slug = exfields.AutoSlugField(populate_from='label', blank=True)
     position = PositionField()
-    
+
     def __unicode__(self):
         return self.label
+
     class Meta:
         verbose_name = _(u'tag category')
         verbose_name_plural = _(u'tag categories')
@@ -27,22 +28,25 @@ class Ctag(TagBase):
     Defines a Common Tag resource
     '''
     # FIELDS name and slug are defined in TagBase    
-    language    = models.CharField(_(u'language'),max_length=10, default='fr')
-    user        = models.ForeignKey(User,blank=True,null=True,verbose_name=_(u'django user'),editable=False)
-    uri         = models.CharField(_(u'tag URI'),blank=True,max_length=250,editable=False)
-    author_uri  = models.CharField(_(u'author URI'),blank=True,max_length=250,editable=False)
-    created     = exfields.CreationDateTimeField(_(u'created'))
-    modified    = exfields.ModificationDateTimeField(_(u'modified'))
-    category = models.ForeignKey(TagCategory,null=True,blank=True,verbose_name=_(u'category'))
-    
+    language = models.CharField(_(u'language'), max_length=10, default='fr')
+    user = models.ForeignKey(User, blank=True, null=True, verbose_name=_(u'django user'), editable=False)
+    uri = models.CharField(_(u'tag URI'), blank=True, max_length=250, editable=False)
+    author_uri = models.CharField(_(u'author URI'), blank=True, max_length=250, editable=False)
+    created = exfields.CreationDateTimeField(_(u'created'))
+    modified = exfields.ModificationDateTimeField(_(u'modified'))
+    category = models.ForeignKey(TagCategory, null=True, blank=True, verbose_name=_(u'category'))
+
     def get_absolute_url(self):
         return reverse('tag_detail', args=[self.slug])
+
     class Meta:
         verbose_name = _(u'tag')
         verbose_name_plural = _(u'tags') 
            
+
 class CtaggedItem(GenericTaggedItemBase):
     tag = models.ForeignKey(Ctag, related_name="ctagged_items")
+    
     class Meta:
         verbose_name = _(u'tagged item')
         verbose_name_plural = _(u'tagged items')
