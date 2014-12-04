30c2441aec855a4e5a3b1ba91e73484bdfa1a45b
Dominique Guardiola <dguardiola@quinode.fr>
2014-12-04 17:43:29 +0100

    optional slash

--- coop_tag/urls.py
+++ coop_tag/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls.defaults import patterns, url, include
 
 urlpatterns = patterns('coop_tag.views',
-    url(r'^tag/(?P<slug>[\w-]+)/$', 'tag_detail', name="tag_detail"),
+    url(r'^tag/(?P<slug>[\w-]+)/?$', 'tag_detail', name="tag_detail"),
     url(r'^list/$', 'list_tags', name='tag-autosuggest-list'),
 )
 
